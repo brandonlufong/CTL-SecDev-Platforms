e31aca5851d8d288d71919731d6b138d652d14df
brandonlufong <35339522+brandonlufong@users.noreply.github.com>
2025-10-01 15:23:35 +0100

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const PORT = 5002;
 const HOST = '172.20.19.172';
-const API_BASE_URL = `http://${HOST}:${PORT}`;
+const API_BASE_URL = `http://172.20.19.172:${PORT}`;
 
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
